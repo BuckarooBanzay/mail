c1f616e59f95bc1b3cb6d9a5e2778fde9159c108
Thomas Rudin <thomas@rudin.io>
2018-12-30 21:02:43 +0100

    port as parameter

--- webmail/main.go
+++ webmail/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"log"
+	"flag"
+	"strconv"
 	"net/http"
 )
 
 func main() {
+	var port int
+
+	flag.IntVar(&port, "port", 8080, "Port to listen on")
+	flag.Parse()
 	
+	log.Print("Listening on port ", port)
+
 	http.Handle("/", http.FileServer(FS(false)))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":" + strconv.Itoa(port), nil))
 }
 
