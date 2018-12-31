d692e35c91d419e7a4be1e6709b8a0e9ade3a523
Thomas Rudin <thomas@rudin.io>
2018-12-31 12:53:03 +0100

    logger, mux and private/public port

--- webmail/main.go
+++ webmail/main.go
@@ -7,15 +7,26 @@ import (
 	"net/http"
 )
 
-func main() {
-	var port int
 
-	flag.IntVar(&port, "port", 8080, "Port to listen on")
+func logger(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s requested %s", r.RemoteAddr, r.URL)
+		h.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	var publicport, privateport int
+
+	flag.IntVar(&publicport, "publicport", 8080, "Public port to listen on")
+	flag.IntVar(&privateport, "privateport", 8081, "Private port to listen on")
 	flag.Parse()
 	
-	log.Print("Listening on port ", port)
+	log.Print("Listening on ports: public=", publicport, " private=", privateport)
 
-	http.Handle("/", http.FileServer(FS(false)))
-	log.Fatal(http.ListenAndServe(":" + strconv.Itoa(port), nil))
+	h := http.NewServeMux()
+	h.Handle("/", http.FileServer(FS(false)))
+
+	log.Fatal(http.ListenAndServe(":" + strconv.Itoa(publicport), logger(h)))
 }
 
